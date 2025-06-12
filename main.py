8a68bc5fb83080ecea1297780f22e9f0135c77f4
Sohex <616447sohanghosh@gmail.com>
2025-06-12 22:57:11 +0530

    hoejapls

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("models/gemini-pro")
 
 app = FastAPI()
 
